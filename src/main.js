96804e4ada1c90688d5d4e0401591099beee817f
vrbdas <vrbdas@gmail.com>
2024-11-07 10:23:09 +0500

    end lesson

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.scss';
-
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
 import App from './App.vue';
 
-import router from './router';
-
+const pinia = createPinia();
 const app = createApp(App);
+
+app.use(pinia);
 app.use(router);
 app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <script setup>
-  import { onBeforeRouteLeave } from 'vue-router';
-  onBeforeRouteLeave(() => confirm('Are you sure to leave?'));
+import { onBeforeRouteLeave } from 'vue-router';
+import { useCounterStore } from '@/stores/counter';
+const counter = useCounterStore();
+
+onBeforeRouteLeave(() => confirm('Are you sure to leave?'));
 </script>
 
 <template>
   <div class="card">
     <h1 class="title">Home page</h1>
     <p>From this page you can go to any page and read prepared emails.</p>
+    <button class="btn" @click="counter.count++">Count</button>
+    <p>Counter value: {{ counter.count }}</p>
     <RouterLink to="/mail">
       <button class="btn">Go to emails</button>
     </RouterLink>
   </div>
 </template>
 
-<style scoped lang='scss'>
-
-</style>
+<style scoped lang="scss"></style>
